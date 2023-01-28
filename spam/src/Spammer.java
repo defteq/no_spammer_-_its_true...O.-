46e0af4831215d3e6d41af3c4b53617d73723d99
defteq <118099308+defteq@users.noreply.github.com>
2023-01-29 00:32:57 +0800

    Add files via upload

---
+++ spam/src/Spammer.java
@@ -0,0 +1,47 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
+public class Spammer {
+
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        int x = 1;
+
+        Robot robot = new Robot();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Текст:");
+        String Text = scanner.nextLine();
+
+        StringSelection stringSelection = new StringSelection(Text);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+
+        System.out.println("Подождите 5 секунд.");
+        Thread.sleep(5000);
+
+        while(x <= 100) {
+
+            Thread.sleep(10);
+
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_V);
+
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+
+
+            x++;
+
+        }
+
+    }
+
+}
+
